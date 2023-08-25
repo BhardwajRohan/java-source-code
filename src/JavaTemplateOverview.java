0abf87dc9a2b5ef4126ecfe6c2823b081cd05499
ROUNAK BHARDWAJ <rounakkumar1034@gmail.com>
2023-08-26 00:02:21 +0530

    Added java file. in which one by one written code has been explained

---
+++ src/JavaTemplateOverview.java
@@ -0,0 +1,13 @@
+//importing classes from packagee
+import java.util.*;
+
+//Main class - JavaTemplateOverview
+public class JavaTemplateOverview {
+
+    //Main driver method
+    public static void main(String[] args){
+
+        //print statement
+        System.out.println("Wellcome to my first java program");
+    }
+}
