471e41ec4c9ba0f25347886379d617a1fda5c93b
ROUNAK BHARDWAJ <rounakkumar1034@gmail.com>
2023-09-02 01:31:00 +0530

    find largest number problem solved

---
+++ src/LargestNum_06.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LargestNum_06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        //Using Math.max function to calculate largest num; 
+        int max = Math.max(c,(Math.max(a,b)));
+        System.out.println("Max = " + max);
+
+    }
+}
